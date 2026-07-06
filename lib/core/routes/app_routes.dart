5832c0357c7b311784eeb0638b6def5368b96c23
Gabrielly Lopes Pontes <gabriellylopesponntes@gmail.com>
2026-07-05 23:57:13 -0300

    feat[routes]: routes implemented

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const splash = '/';
+  static const home = '/home';
+  static const students = '/students';
+  static const studentForm = '/students/form';
+  static const studentDetails = '/students/details';
+  static const ranking = '/ranking';
+  static const about = '/about';
+  
+}
